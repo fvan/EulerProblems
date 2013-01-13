3ecb8ce4356572d454e02d701bb6c3b388d27c40
fvan <frankvand@gmail.com>
2013-01-13 17:32:49 -0500

    Refactored the calculation of your current list of primes

--- src/Primes.java
+++ src/Primes.java
@@ -7,41 +7,36 @@ public class Primes {
 		return extendOrderedPrimeList(new ArrayList<Integer>(), 2, primesTo);
 	}
 
-
 	public List<Integer> extendOrderedPrimeList(List<Integer> orderedPrimes, int startAt, int primesUpTo) {
 		int valuesBetweenStartAndEnd = 1 + primesUpTo - startAt;
 		boolean[] booleanArray = booleanArray(valuesBetweenStartAndEnd);
-		
+
 		for (int existingPrime : orderedPrimes) {
-			if(existingPrime > Math.sqrt(primesUpTo)) {
+			if (existingPrime > Math.sqrt(primesUpTo)) {
 				break;
 			}
-			for (int valueToEliminate = existingPrime * existingPrime; valueToEliminate <= primesUpTo; valueToEliminate = valueToEliminate + existingPrime) {
-				int arrayIndex = valueToEliminate - startAt;
-				if(arrayIndex < 0) {
-					continue;
-				}
-				booleanArray[arrayIndex] = false;
+			for (int indexToEliminate = existingPrime * existingPrime - startAt + ((int) Math.ceil(startAt/ ((double) existingPrime)) - existingPrime) * existingPrime ; indexToEliminate <= primesUpTo - startAt; indexToEliminate = indexToEliminate + existingPrime) {
+				booleanArray[indexToEliminate] = false;
 			}
 		}
-		
+
 		for (int currentArrayPosition = 0; currentArrayPosition < booleanArray.length; currentArrayPosition++) {
 			int currentValue = currentArrayPosition + startAt;
-			if(currentValue > Math.sqrt(primesUpTo)) {
+			if (currentValue > Math.sqrt(primesUpTo)) {
 				break;
 			}
-			if(!booleanArray[currentArrayPosition]) {
-				continue;
+			if (booleanArray[currentArrayPosition]) {				
+				for (int i = currentValue * currentValue - startAt; i < booleanArray.length; i = i + currentValue) {
+					booleanArray[i] = false;
+				}
 			}
-			for (int i = currentValue * currentValue - startAt; i < booleanArray.length; i = i + currentValue) {
-				booleanArray[i] = false;
-			}
+
 		}
-		
+
 		orderedPrimes.addAll(convertToInOrderIncludeValuesWithOffset(booleanArray, startAt));
 		return orderedPrimes;
 	}
-	
+
 	private List<Integer> convertToInOrderIncludeValuesWithOffset(boolean[] inclusionIndicator, int offset) {
 		ArrayList<Integer> includedValues = new ArrayList<Integer>();
 		for (int i = 0; i < inclusionIndicator.length; i++) {
